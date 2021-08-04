8b7402dde5766793ba6048068ee9e27bb897dac6
Ultra24 <62357227+Ultra24@users.noreply.github.com>
2021-08-04 11:25:00 -0400

    Added variable containing the quotes in an array

--- src/app.js
+++ src/app.js
@@ -1,1 +1,73 @@
+let quotes = [ ` "The only way to do great work is to love what you do. If you haven't found it yet, keep looking. Don't settle. 
+        As with all matters of the heart, you'll know when you find it. And -- like any great relationship -- it gets better and better as the years roll on. 
+        So keep looking. Don't settle. - Steve Jobs"`,
+         `"I'm convinced that about half of what seperates successful entrepenurs from the non-successful ones is pure perseverance. 
+        Unless you have a lot of passion about this, you're not gonna survive. You're going to give up.
+        So you've got to have an idea, or a problem or a wrong that you want to right that you're passionate about; 
+        otherwise, you're not going to have the perseverance to stick it through. - Steve Jobs"`,  
+        "Sometimes life hits you in the head with a brick. Don't lose faith. - Steve Jobs",
+    `"For the past thirty-three years of my life, I have looked in the mirror every morning and asked myself:
+     \"If today were the last day of my life, would I want to do what I am about to do today?\"
+      And whenever the answer has been \"No\" for too many days in a row, I know I need to change something. - Steve Jobs" `, 
+       
+         ` "Here's to the crazy ones. The misfits. The rebels. 
+            The troublemakers. The round pegs in the square holes.
+             The ones who see differently. They're not fond of rules. 
+            And they have no respect of the status-quo.
+            You can quote them, disagree with them, glorify or vilify them.
+            About the only thing that you can't do is ignore them.
+            Because they change things. They push the human race forward.
+            While some see them as the crazy ones, we see genius.
+            Because the people who are crazy enough to think they can change the world, are the ones who do."
+            - Apple Computers, Inc. `, 
+       
+            "Those who can imagine anything, can create the impossible. - Alan Turing",
+            "Sometimes it is the people who no one imagines anything of who do the things that no one can imagine. - Alan Turing", 
+       
+            "Logic will get you from A to Z; Imagination will get you everywhere. - Albert Einstein",
+            "In the middle of every difficulty lies an opportunity. - Albert Einstein",
+            "Try not to become a man of success. Rather become a man of value - Albert Einstein.",
+            "Make everything as simple as possible, but not simpler - Albert Einstein",
+            "Play is the highest form of research - Albert Einstein", 
+            "Wisdom is not a product of schooling but of the lifelong attempt to acquire it - Albert Einstein", 
+       
+            "I don't care that they stole my idea, I care that they don't have any of their own. - Nikola Tesla",
+            "Be alone, that is the secret of invention; be alone, that is when ideas are born - Nikola Tesla",  
+       
+            "Pleasure in the job puts perfection in the work. - Aristotle",
+            "Patience is bitter, but its fruit is sweet. - Aristotle",
+            "Quality is not an act, it is a habit. - Aristotle", 
+        
+            "Real kowledge is to know the extent of one's ignornace. - Confucius",
+            "Our greatest glory is not in never falling, but in rising every time we fall. - Confucius",
+            "It does not matter how slowly you go as long as you do not stop - Confucius",
+         
+            "I know nothing except the fact of my ignorance. - Socrates",
+            "Wisdom begins in wonder. - Socrates",
+            "To find yourself, think for yourself. - Socrates",
+         
+            "The way to get started is to quit talking and begin doing. - Walt Disney",
+            "If you can dream it, you can do it. - Walt Disney",    
+         
+            "A winner is a dreamer who never gives up - Nelson Mandela",
+            "It always seems impossible until it's done - Nelson Mandela",
+         
+            "If you are going through hell, keep going. - Winston Churchill",
+            "Success is not final, failure is not fatal; it is the courage to continue that counts. - Winston Churchill",
+          
+            "The best way to predict your future is to create it. - Abraham Lincoln",
+            "Whatever you are, be a good one. - Abraham Lincoln",
+        
+            "A man is but a product of his thoughts, what he thinks, he becomes. - Mahatma Gandhi.",
+        
+            "It is not the strongest of the species that survive, nor the most intelligent, but the one most responsive to change. - Charles Darwin",
+        
+            "The time is always right to do the right thing. - Martin Luther King Jr.",   
+            
+            "The best revenge is massive success. - Frank Sinatra",
 
+            "The journey of a thousand miles begins with one step. - Lao Tzu",
+
+            "Life is what happens when you’re busy making other plans. - John Lennon",
+
+];
